28f120186ca6d8d7a9140be37fdabb617425337f
Maarten Smeyers <maartensmeyers@gmail.com>
2021-12-02 07:11:14 +0100

    day 1 solution

--- day01/part2.py
+++ day01/part2.py
@@ -7,6 +7,7 @@ entries = list(map(int, entries))
 
 total = 0
 for x in range(len(entries) - 3):
+    # Due to the overlap in the sliding window we could have used entries[x] < entries[x + 3]
     if entries[x] + entries[x + 1] + entries[x + 2] < entries[x + 1] + entries[x + 2] + entries[x + 3]:
         total = total + 1
 
